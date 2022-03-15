da85dbd5903b4b56ee9e75359da93500953ac02f
Felipe Bertho <60559125+felipebertho@users.noreply.github.com>
2022-03-15 17:19:44 +0000

    "Email automaticos programados"

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import yagmail
 import os
 import time
-
+from datetime import datetime as dt
 #remetente e destinatário
 sender = 'fhbertho@gmail.com'
 receiver = 'felipebertho@icloud.com'
@@ -11,8 +11,12 @@ subject = "Teste de envio do script"
 contents = "Esse é o conteúdo do e-mail"
 
 #configuração da senha do e-mail para utilização do script.
-yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWD'))
 
-yag.send(to=receiver, subject=subject, contents=contents)
-
-print("E-mail enviado! ")
+while True:
+  now = dt.now()
+  if dt.now().hour == 13 and dt.now().minute == 50:
+    
+    yag = yagmail.SMTP(user=sender,                              password=os.getenv('PASSWD'))
+    yag.send(to=receiver, subject=subject,                       contents=contents)
+    print("E-mail enviado! ")
+    time.sleep(60)
